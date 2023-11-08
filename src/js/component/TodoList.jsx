c5b1b001d08cecfe0d46b700552c82394d360103
Pau Fargas <142971403+PauFargas@users.noreply.github.com>
2023-11-08 11:56:18 +0000

    [FEAT] Todo List

---
+++ src/js/component/TodoList.jsx
@@ -0,0 +1,40 @@
+import React, {useState} from "react";
+
+
+export const TodoList = () => {
+    const [inputValue , setInputValue] = useState("");
+
+    const [todos , setTodos] = useState ([]);
+
+    const handleKeyPress = (e) => {
+        if (e.key === "Enter") {
+          const updatedTodos = [inputValue, ...todos];
+          setTodos(updatedTodos);
+          setInputValue("");
+        };
+    }
+
+    return (
+        <div className = "container">
+            <h1>My Todo's</h1> 
+            <ul>
+                <li> 
+                    <input type="text" 
+                    onChange = {(e)=> setInputValue (e.target.value)}
+                    value = {inputValue} 
+                    onKeyPress= {handleKeyPress}
+                    placeholder = "Add to list"/>
+                </li>
+              
+                {todos.map((t, index) =>( 
+                <li key={index} className={index !== undefined ? "fade-out" : ""}>
+                    {t} <i class="fas fa-times" onClick ={() => setTodos(todos.filter((t, currentIndex) => index != currentIndex))}></i>
+                </li>))}
+               
+                
+            </ul>
+            <div>{todos.length} Tasks</div>
+        
+        </div>
+    )
+};
