29311f2a1204e15c97d32528843d8ffa79c7db0b
Pau Fargas <142971403+PauFargas@users.noreply.github.com>
2023-11-08 20:19:37 +0000

    [Final]

--- src/js/component/TodoList.jsx
+++ src/js/component/TodoList.jsx
@@ -1,40 +1,47 @@
-import React, {useState} from "react";
-
+import React, { useState } from "react";
 
 export const TodoList = () => {
-    const [inputValue , setInputValue] = useState("");
+  const [inputValue, setInputValue] = useState("");
+  const [todos, setTodos] = useState([]);
 
-    const [todos , setTodos] = useState ([]);
-
-    const handleKeyPress = (e) => {
-        if (e.key === "Enter") {
-          const updatedTodos = [inputValue, ...todos];
-          setTodos(updatedTodos);
-          setInputValue("");
-        };
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter" && inputValue.trim() !== "") {
+      const updatedTodos = [inputValue, ...todos];
+      setTodos(updatedTodos);
+      setInputValue("");
     }
+  };
 
-    return (
-        <div className = "container">
-            <h1>My Todo's</h1> 
-            <ul>
-                <li> 
-                    <input type="text" 
-                    onChange = {(e)=> setInputValue (e.target.value)}
-                    value = {inputValue} 
-                    onKeyPress= {handleKeyPress}
-                    placeholder = "Add to list"/>
-                </li>
-              
-                {todos.map((t, index) =>( 
-                <li key={index} className={index !== undefined ? "fade-out" : ""}>
-                    {t} <i class="fas fa-times" onClick ={() => setTodos(todos.filter((t, currentIndex) => index != currentIndex))}></i>
-                </li>))}
-               
-                
-            </ul>
-            <div> <h6>{todos.length} Tasks</h6></div>
+  return (
+    <div className="container">
+      <h1>My ToDo's</h1>
+      <ul>
+        <li>
+          <input
+            type="text"
+            onChange={(e) => setInputValue(e.target.value)}
+            value={inputValue}
+            onKeyPress={handleKeyPress}
+            placeholder="Add to list"
+          />
+        </li>
+
+        {todos.map((t, index) => (
+          <li key={index}>
+            {t}{" "}
+            <i
+              className="fas fa-times"
+              onClick={() =>
+                setTodos(todos.filter((_, currentIndex) => index !== currentIndex))
+              }
+            ></i>
+          </li>
+        ))}
+      </ul>
+      <div>
         
-        </div>
-    )
+        <div><h6>{todos.length} Tasks</h6></div>
+      </div>
+    </div>
+  );
 };
